b965dca98c3b2bca8954b15d900039bb88ec08c5
Becca Byrne <aggiebec06@gmail.com>
2022-02-09 08:35:12 -0600

    added controllers for user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -8,7 +8,57 @@ const userController = {
         .catch(err => {
             console.log(err);
             res.status(400).json(err);
+        });
+    },
+
+    // get one user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .then(dbUserData => {
+            //if no user found, send 404
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this ID' });
+                return;
+            }
+            res.json(dbUserData);
         })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // create User
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+    },
+
+    // update user by ID
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    
+    // delete user
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found wtih this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
     }
 }
 
